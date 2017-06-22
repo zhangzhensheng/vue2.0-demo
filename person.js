f23f00ac2f1e309f2c2206cbe91a60b0163c035e
zhangzhensheng <3200424468@qq.com>
2017-06-22 21:06:56 +0800

    Update person.js

--- person.js
+++ person.js
@@ -1,7 +1,7 @@
 {
 "sites": [
-{ "name":"菜鸟教程" , "url":"www.runoob.com" }, 
-{ "name":"google" , "url":"www.google.com" }, 
-{ "name":"微博" , "url":"www.weibo.com" }
+{ "name":"菜鸟教程" , "url":"www.runoob.com111" }, 
+{ "name":"google" , "url":"www.google.com222" }, 
+{ "name":"微博" , "url":"www.weibo.com333" }
 ]
 }
